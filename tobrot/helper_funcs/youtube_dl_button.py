8277d4bce1955cbca7f597bdf792518b8e32ba55
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-08-15 16:43:41 +0300

    Update youtube_dl_button.py

--- tobrot/helper_funcs/youtube_dl_button.py
+++ tobrot/helper_funcs/youtube_dl_button.py
@@ -24,7 +24,6 @@ from tobrot import (
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-from tobrot.helper_funcs.extract_link_from_message import extract_link
 from tobrot.helper_funcs.upload_to_tg import upload_to_tg
 
 
@@ -79,41 +78,26 @@ async def youtube_dl_call_back(bot, update):
     #
     youtube_dl_url = response_json.get("webpage_url")
     LOGGER.info(youtube_dl_url)
-    youtube_dl_title = str(response_json.get("title"))
-    if "/" in youtube_dl_title:
-        youtube_dl_title = youtube_dl_title.replace("/", "_")
-    custom_file_name = youtube_dl_title + \
-        "_" + youtube_dl_format + "." + youtube_dl_ext
+    #
+    custom_file_name = "%(title)s.%(ext)s"
+    # https://superuser.com/a/994060
     LOGGER.info(custom_file_name)
     #
-    if "noyes.in" in youtube_dl_url or "tor.checker.in" in youtube_dl_url:
-        await bot.edit_message_text(
-            chat_id=update.message.chat.id,
-            text="😡 <i>Tafadhari Usitumie Vibaya <u>Huduma</u> Hii ya  BURE</i> 🌚",
-            message_id=update.message.message_id
-        )
-        return
-    if "drive.google.com" in youtube_dl_url and youtube_dl_format != "source":
-        await bot.edit_message_text(
-            chat_id=update.message.chat.id,
-            text="<i>tafadhali usitumie vibaya huduma hii ya BURE</i>",
-            message_id=update.message.message_id
-        )
-        return
-    #
-    await bot.edit_message_text(
-        text="❍ Nina Download",
-        chat_id=update.message.chat.id,
-        message_id=update.message.message_id
+    await update.message.edit_caption(
+        caption="❍ Nina Download"
     )
-    description = "@PublicLeechGroup"
+    description = "@PublicLeech"
     if "fulltitle" in response_json:
         description = response_json["fulltitle"][0:1021]
         # escape Markdown and special characters
     #
-    tmp_directory_for_each_user = DOWNLOAD_LOCATION + "/" + str(update.from_user.id)
+    tmp_directory_for_each_user = os.path.join(
+        DOWNLOAD_LOCATION,
+        str(update.from_user.id)
+    )
     if not os.path.isdir(tmp_directory_for_each_user):
         os.makedirs(tmp_directory_for_each_user)
+    download_directory = tmp_directory_for_each_user
     download_directory = os.path.join(tmp_directory_for_each_user, custom_file_name)
     command_to_exec = []
     if tg_send_type == "audio":
@@ -153,6 +137,7 @@ async def youtube_dl_call_back(bot, update):
     command_to_exec.append("--no-warnings")
     # command_to_exec.append("--quiet")
     command_to_exec.append("--restrict-filenames")
+    #
     if "hotstar" in youtube_dl_url:
         command_to_exec.append("--geo-bypass-country")
         command_to_exec.append("IN")
@@ -173,10 +158,8 @@ async def youtube_dl_call_back(bot, update):
     ad_string_to_replace = "please report this issue on https://yt-dl.org/bug . Make sure you are using the latest version; see  https://yt-dl.org/update  on how to update. Be sure to call youtube-dl with the --verbose flag and include its complete output."
     if e_response and ad_string_to_replace in e_response:
         error_message = e_response.replace(ad_string_to_replace, "")
-        await bot.edit_message_text(
-            chat_id=update.message.chat.id,
-            message_id=update.message.message_id,
-            text=error_message
+        await update.message.edit_caption(
+            caption=error_message
         )
         return False, None
     if t_response:
@@ -184,26 +167,19 @@ async def youtube_dl_call_back(bot, update):
         # os.remove(save_ytdl_json_path)
         end_one = datetime.now()
         time_taken_for_download = (end_one -start).seconds
-        dir_contents = os.listdir(tmp_directory_for_each_user)
-        dir_contents.sort()
-        download_directory = ""
-        if len(dir_contents) >= 1:
-            download_directory = os.path.join(tmp_directory_for_each_user, dir_contents[0])
-        else:
-            await bot.edit_message_text(
-                chat_id=update.message.chat.id,
-                message_id=update.message.message_id,
-                text=t_response + "\n" + e_response
-            )
-            return False, None
-        file_size = os.stat(download_directory).st_size
+        dir_contents = len(os.listdir(tmp_directory_for_each_user))
+        # dir_contents.sort()
+        await update.message.edit_caption(
+            caption=f"found {dir_contents} files"
+        )
         user_id = update.from_user.id
         #
         final_response = await upload_to_tg(
             update.message,
-            download_directory,
+            tmp_directory_for_each_user,
             user_id,
-            {}
+            {},
+            True
         )
         LOGGER.info(final_response)
         #
@@ -212,26 +188,3 @@ async def youtube_dl_call_back(bot, update):
         except:
             pass
         #
-        message_to_send = ""
-        for key_f_res_se in final_response:
-            local_file_name = key_f_res_se
-            message_id = final_response[key_f_res_se]
-            channel_id = str(AUTH_CHANNEL)[4:]
-            private_link = f"https://t.me/c/{channel_id}/{message_id}"
-            message_to_send += "👉 <a href='"
-            message_to_send += private_link
-            message_to_send += "'>"
-            message_to_send += local_file_name
-            message_to_send += "</a>"
-            message_to_send += "\n"
-        if message_to_send != "":
-            mention_req_user = f"<a href='tg://user?id={user_id}'>Your Requested Files</a>  @Huduma\n\n"
-            message_to_send = mention_req_user + message_to_send
-            message_to_send = message_to_send + "\n\n" + "☻ @Mawaidha1"
-        else:
-            message_to_send = "<i>Ime FAILED</i> Ku upload files Lako. 😞😞"
-        await update.message.reply_to_message.reply_text(
-            text=message_to_send,
-            quote=True,
-            disable_web_page_preview=True
-        )
